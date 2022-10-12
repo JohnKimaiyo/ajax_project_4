a5369781de587f4369180b1e959b472103d3ae49
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-12 11:47:42 +0300

    seting up the post http request

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
+import { brainhttp } from './brainhttp';
+const serverURL = `htpp:/127.0.0.13001/api/employee`;
 // get button
 let getButton = document.querySelector('#get-btn');
 getButton.addEventListener('click', function () {
     fetchEmployees();
 });
 let fetchEmployees = () => {
-
+    let http = new brainhttp();
+    let url = `${serverURL}/employees`;
+    http.get(url, (err, employees) => {
+        if (err) throw err;
+        let tableRows = '';
+        for (let employee of employees) {
+            tableRows += `<tr>
+            <td>${employee.id}</td>
+            <td>${employee.first_name}</td>
+            <td>${employee.last_name}</td>
+            <td>${employee.email}</td>
+            <td>${employee.gender}</td>
+            <td>${employee.ip_address}</td>
+</tr>
+        `;
+        }
+        document.querySelector('#table-body').innerHTML - tableRows
+    });
+    console.log(users);
 }
+
+// post button
+let postButton = document.querySelector('#post-btn');
+postButton.addEventLisstener('click', function () {
+    let url = `${serverURL}/employees`;
+    let employee = {
+        first_name: 'john',
+        last_name: 'doe',
+        email: 'johndoe@gmail',
+        gender: 'male',
+        ip_address: '127.0.0.5'
+    };
+    let http = new brainhttp();
+    http.post(url, employees, (data) => {
+        alert(JSON.stringfly(data));
+        fetchEmployees();
+    })
+})
